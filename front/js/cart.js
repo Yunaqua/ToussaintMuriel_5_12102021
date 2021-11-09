e2e3cafe1f47266d6e789803ef0240d6a945609b
Muriel <toussaintmuriel@yahoo.com>
2021-11-09 14:59:34 +0100

    suppression d'un élement du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -8,7 +8,7 @@ console.log(produitEnregistrerStorage);
 for (let jsonPanier of produitEnregistrerStorage){
     
     const product = new Product(jsonPanier);
-    document.querySelector('#cart__items').innerHTML += `<article class="cart__item" data-id="${product.id}">
+    document.querySelector('#cart__items').innerHTML += `<article class="cart__item" data-id="${product.id}" data-color="${product.couleur}">
                                                             <div class="cart__item__img">
                                                                 <img src="${product.image}" alt="${product.alt}">
                                                             </div>
@@ -23,46 +23,64 @@ for (let jsonPanier of produitEnregistrerStorage){
                                                                 <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.nombre_article}">
                                                                 </div>
                                                                 <div class="cart__item__content__settings__delete">
-                                                                <p class="deleteItem" onclick="myFunction(produitEnregistrerStorage)">Supprimer</p>
+                                                                <p class="deleteItem">Supprimer</p>
                                                                 </div>
                                                             </div>
                                                             </div>
                                                         </article>`;
-         
-       
-   
- /*  const supprimerArticle = document.querySelector(".deleteItem");
+                                              
+
+  
+  /*const supprimerArticle = document.querySelector(".deleteItem");
    supprimerArticle.addEventListener("click",(event) => {
-    alert ("Hello World!");
-        //produitEnregistrerStorage.removeItem(product);
+    //alert ("Hello World!");
         console.log(product);
-        console.log("yo");
-    })//event supprimer
-                                */                      
-    console.log(product);                                                     
+    })//event supprimer */
+                                                   
   } //for
-  function myFunction(e){
+ 
+  
+  function myElements(){
+    let deleteItem = document.querySelectorAll(".deleteItem");
+    for(let i=0; i < deleteItem.length; i++){
+      deleteItem[i].addEventListener("click",(event) => {
+        const getId = event.path[4].getAttribute("data-id");
+        const getColor = event.path[4].getAttribute("data-color");
 
-    const isLargeNumber = (element) => e;
+        let check = produitEnregistrerStorage.some( e => e.id == getId && e.couleur == getColor)
+    console.log("yo");
+    if(check){
+      let suppresionStorage = produitEnregistrerStorage.findIndex( e => e.id == getId && e.couleur == getColor) //recupère l'index dans le storage
+      console.log(suppresionStorage);
+      produitEnregistrerStorage.splice(suppresionStorage,1);
+      console.log(produitEnregistrerStorage);
+      localStorage.setItem('produit', JSON.stringify(produitEnregistrerStorage));
+    }
+      });
+    }//for
 
-    console.log(produitEnregistrerStorage.findIndex(isLargeNumber));
-    console.log(e);
-    //produitEnregistrerStorage.removeItem(product);
-} 
+    
 
-  const ensemblePanier = document.querySelector("#cart__items");
-    ensemblePanier.addEventListener('change',(event) => {
+} //myElements
+myElements();
 
-    var articleTotal =0; 
+function calculPanier(){
+  var articleTotal =0; 
     var prixTotal =0;                                               
     for(let i = 0; i < produitEnregistrerStorage.length; i++){
             articleTotal += parseInt(produitEnregistrerStorage[i].nombre_article);
             prixTotal += parseInt(produitEnregistrerStorage[i].prix)*(produitEnregistrerStorage[i].nombre_article);
-            //console.log(articleTotal);
              
           } //for
     document.querySelector('#totalQuantity').innerHTML = articleTotal;
     document.querySelector('#totalPrice').innerHTML = prixTotal;
+}
+calculPanier();
+
+  const ensemblePanier = document.querySelector("#cart__items");
+    ensemblePanier.addEventListener('change',(event) => {
+      calculPanier();
+    
 })//fin event
 
   
